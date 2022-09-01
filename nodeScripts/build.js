c1e5041034a7e02743f8ed64884c6a15ab27f29e
WebHelix <andrew.shea@webhelix.dev>
2022-09-01 21:01:23 +0000

    Updated build.js

--- nodeScripts/build.js
+++ nodeScripts/build.js
@@ -1,64 +1,41 @@
-const path = require("path")
-const { existsSync, readdirSync, lstatSync} = require("fs")
-const { execSync } = require('child_process')
-const { exit } = require('node:process')
+const { existsSync } = require("fs");
+const { execSync } = require("child_process");
+const { exit } = require("node:process");
+const { findFiles } = require("./findFiles");
 
-
-const htmlFilesArray = []
-const jsFilesArray = [] 
-
-const findFiles = (sourcePath, filter, outputArray = []) => {
-
-  if (!existsSync(sourcePath)) {
-    console.log("No directory at this path", sourcePath);
-    return;
-  }
-
-  const files = readdirSync(sourcePath);
-  for (const file of files) {
-    const filename = path.join(sourcePath, file);
-    const stat = lstatSync(filename);
-    if (stat.isDirectory()) {
-      findFiles(filename, filter, outputArray); //recurse
-    } else if (filename.endsWith(filter)) {
-      outputArray.push(filename)
-    }
-  }
-}
+const htmlFilesArray = [];
+const jsFilesArray = [];
 
 findFiles("./pages", ".html", htmlFilesArray);
 findFiles("./js", ".js", jsFilesArray);
 
-
 // console.log(htmlFilesArray)
 
 const copyAllFiles = () => {
-    
-    if (existsSync('./dist/')) {
-        execSync('rm -rf dist/ && mkdir ./dist')
-        htmlFilesArray.forEach((file) => {
-            execSync(`cp ${file} dist/`)
-        })  
-    } else {
-        execSync('mkdir ./dist')
-        htmlFilesArray.forEach((file) => {
-            execSync(`cp ${file} dist/`)
-        }) 
-    }
-    
-    if (existsSync('./dist/js')) {
-        jsFilesArray.forEach((file) => {
-            execSync(`cp ${file} dist/js/`)
-        })
-    } else {
-        execSync('mkdir ./dist/js')
-        jsFilesArray.forEach((file) => {
-            execSync(`cp ${file} dist/js/`)
-        })
-    }
-    
-exit()
-    
-}
+  if (existsSync("./dist/")) {
+    execSync("rm -rf dist/ && mkdir ./dist");
+    htmlFilesArray.forEach((file) => {
+      execSync(`cp ${file} dist/`);
+    });
+  } else {
+    execSync("mkdir ./dist");
+    htmlFilesArray.forEach((file) => {
+      execSync(`cp ${file} dist/`);
+    });
+  }
 
-copyAllFiles()
+  if (existsSync("./dist/js")) {
+    jsFilesArray.forEach((file) => {
+      execSync(`cp ${file} dist/js/`);
+    });
+  } else {
+    execSync("mkdir ./dist/js");
+    jsFilesArray.forEach((file) => {
+      execSync(`cp ${file} dist/js/`);
+    });
+  }
+
+  exit();
+};
+
+copyAllFiles();
